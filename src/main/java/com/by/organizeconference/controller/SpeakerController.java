d9590f2bef336e029093db742b5c89fc9d5834cd
burak-yildirim <yildirim.burak@protonmail.com>
2019-07-09 14:03:07 +0300

    @RequestBody annotation added for update and save methods

--- src/main/java/com/by/organizeconference/controller/SpeakerController.java
+++ src/main/java/com/by/organizeconference/controller/SpeakerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,13 +36,13 @@ public class SpeakerController {
     }
     
     @PostMapping("/speakers")
-    public Speaker save(Speaker speaker){
+    public Speaker save(@RequestBody Speaker speaker){
         speaker.setId(null);
         return speakerService.save(speaker);
     }
     
     @PutMapping("/speakers")
-    public Speaker update(Speaker speaker){
+    public Speaker update(@RequestBody Speaker speaker){
         return speakerService.save(speaker);
     }
     

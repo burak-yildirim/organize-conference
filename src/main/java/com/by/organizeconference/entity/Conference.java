27d37d80c062d6753cc791ea24dfd14441e622c1
burak-yildirim <yildirim.burak@protonmail.com>
2019-07-11 22:09:01 +0300

    Conference class pre-defined

--- src/main/java/com/by/organizeconference/entity/Conference.java
+++ src/main/java/com/by/organizeconference/entity/Conference.java
@@ -1,9 +1,21 @@
 package com.by.organizeconference.entity;
 
+import java.util.List;
+
 /**
  *
  * @author burakY
  */
 public class Conference {
+    
+    private Long id;
+    
+    private String title;
+    
+    private List<Speaker> speakers;
+    
+    private List<Topic> topics;
+    
+    private String organizer;
     
 }

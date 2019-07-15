5153a89e59d74f391cfb44ee423c503532f36a43
burak-yildirim <yildirim.burak@protonmail.com>
2019-07-15 20:13:23 +0300

    topicController is reconfigured to use dto instead of entity

--- src/main/java/com/by/organizeconference/controller/TopicController.java
+++ src/main/java/com/by/organizeconference/controller/TopicController.java
@@ -1,9 +1,12 @@
 package com.by.organizeconference.controller;
 
+import com.by.organizeconference.dto.TopicDTO;
 import com.by.organizeconference.entity.Topic;
 import com.by.organizeconference.service.TopicService;
 import java.util.List;
 import java.util.Map;
+import static java.util.stream.Collectors.toList;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,25 +28,48 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
     
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    private TopicDTO topicEntityToDTO(Topic topic){
+        return modelMapper.map(topic, TopicDTO.class);
+    }
+    
+    private Topic topicDTOToEntity(TopicDTO topicDTO){
+        return modelMapper.map(topicDTO, Topic.class);
+    }
+    
     @GetMapping("/topics")
-    public List<Topic> findAll(){
+    public List<TopicDTO> findAll(){
+        return topicService.findAll().stream().map(this::topicEntityToDTO).collect(toList());
+    }
+    
+    @GetMapping("/topicEntities")
+    public List<Topic> findAllEntity(){
         return topicService.findAll();
     }
     
     @GetMapping("/topics/{id}")
-    public Topic findById(@PathVariable Long id){
-        return topicService.findById(id);
+    public TopicDTO findById(@PathVariable Long id){
+        return topicEntityToDTO(topicService.findById(id));
     }
     
     @PostMapping("/topics")
-    public Topic save(@RequestBody Topic topic){
-        topic.setId(null);
-        return topicService.save(topic);
+    public TopicDTO save(@RequestBody TopicDTO topicDTO){
+        topicDTO.setId(null);
+        
+        Topic topic = topicDTOToEntity(topicDTO);
+        topic = topicService.save(topic);
+        
+        return topicEntityToDTO(topic);
     }
     
     @PutMapping("/topics")
-    public Topic update(@RequestBody Topic topic){
-        return topicService.save(topic);
+    public TopicDTO update(@RequestBody TopicDTO topicDTO){
+        Topic topic = topicDTOToEntity(topicDTO);
+        topic = topicService.save(topic);
+        
+        return topicEntityToDTO(topic);
     }
     
     @DeleteMapping("/topics/{id}")

8466a20a4edba8c31268bff6a473e54b3af2ba76
burak-yildirim <yildirim.burak@protonmail.com>
2019-07-17 19:10:56 +0300

    ConferenceController is updated with Converter and Piper

--- src/main/java/com/by/organizeconference/controller/ConferenceController.java
+++ src/main/java/com/by/organizeconference/controller/ConferenceController.java
@@ -1,12 +1,13 @@
 package com.by.organizeconference.controller;
 
+import com.by.organizeconference.converter.Converter;
 import com.by.organizeconference.dto.ConferenceDTO;
 import com.by.organizeconference.entity.Conference;
+import static com.by.organizeconference.helper.Piper.pipe;
 import com.by.organizeconference.service.ConferenceService;
 import java.util.List;
 import java.util.Map;
 import static java.util.stream.Collectors.toList;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,42 +30,40 @@ public class ConferenceController {
     private ConferenceService conferenceService;
     
     @Autowired
-    private ModelMapper modelMapper;
+    private Converter<Conference, ConferenceDTO> converter;
     
-    private ConferenceDTO conferenceEntityToDTO(Conference conference){
-        return modelMapper.map(conference, ConferenceDTO.class);
-    }
-    
-    private Conference conferenceDTOToEntity(ConferenceDTO conferenceDTO){
-        return modelMapper.map(conferenceDTO, Conference.class);
-    }
     
     @GetMapping("/conferences")
     public List<ConferenceDTO> findAll(){
-        return conferenceService.findAll().stream().map(this::conferenceEntityToDTO).collect(toList());
+        return conferenceService.findAll().stream()
+                .map(converter.entityToDto(ConferenceDTO.class)).collect(toList());
     }
     
     @GetMapping("/conferences/{id}")
     public ConferenceDTO findById(@PathVariable Long id){
-        return conferenceEntityToDTO(conferenceService.findById(id));
+        return converter.entityToDto(ConferenceDTO.class).apply(conferenceService.findById(id));
+//        return converter.entityToDto(ConferenceDTO.class).apply(new String()); // this gives error, but shouldnt  
     }
     
     @PostMapping("/conferences")
     public ConferenceDTO save(@RequestBody ConferenceDTO conferenceDTO){
         conferenceDTO.setId(null);
         
-        Conference conference = conferenceDTOToEntity(conferenceDTO);
-        conference = conferenceService.save(conference);
-        
-        return conferenceEntityToDTO(conference);
+        return (ConferenceDTO) pipe(
+                converter.dtoToEntity(Conference.class), 
+                e -> conferenceService.save((Conference) e), 
+                converter.entityToDto(ConferenceDTO.class)
+        ).apply(conferenceDTO);
     }
     
     @PutMapping("/conferences")
     public ConferenceDTO update(@RequestBody ConferenceDTO conferenceDTO){
-        Conference conference = conferenceDTOToEntity(conferenceDTO);
-        conference = conferenceService.save(conference);
-        
-        return conferenceEntityToDTO(conference);
+
+        return (ConferenceDTO) pipe(
+                converter.dtoToEntity(Conference.class),
+                e -> conferenceService.save((Conference) e),
+                converter.entityToDto(ConferenceDTO.class)
+        ).apply(conferenceDTO);
     }
     
     @DeleteMapping("/conferences/{id}")

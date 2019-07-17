304aff2fc9aa6c08f5cd5ea7e00a3c0906d8f3fd
burak-yildirim <yildirim.burak@protonmail.com>
2019-07-17 19:23:24 +0300

    javadoc added for Converter

--- src/main/java/com/by/organizeconference/converter/Converter.java
+++ src/main/java/com/by/organizeconference/converter/Converter.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- *
+ * 'E' for entity and 'D' for dto
  * @author burakY
  */
 @Component

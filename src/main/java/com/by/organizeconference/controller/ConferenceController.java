a8bef57b55ec2d32cb18b1fe7f067aab30c91738
burak-yildirim <yildirim.burak@protonmail.com>
2019-07-29 20:54:35 +0300

    Topic and Conference controllers updated with Merger and reMap. Also partialUpdate feature is added

--- src/main/java/com/by/organizeconference/controller/ConferenceController.java
+++ src/main/java/com/by/organizeconference/controller/ConferenceController.java
@@ -1,16 +1,19 @@
 package com.by.organizeconference.controller;
 
-import com.by.organizeconference.converter.Converter;
+import com.by.organizeconference.converter.OldConverter;
 import com.by.organizeconference.dto.ConferenceDTO;
 import com.by.organizeconference.entity.Conference;
-import static com.by.organizeconference.helper.Piper.pipe;
+import static com.by.organizeconference.utility.Piper.pipe;
 import com.by.organizeconference.service.ConferenceService;
+import com.by.organizeconference.utility.Merger;
+import com.remondis.remap.Mapper;
 import java.util.List;
 import java.util.Map;
 import static java.util.stream.Collectors.toList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,40 +33,49 @@ public class ConferenceController {
     private ConferenceService conferenceService;
     
     @Autowired
-    private Converter<Conference, ConferenceDTO> converter;
+    private Mapper<Conference, ConferenceDTO> conferenceEDMapper;
     
+    @Autowired
+    private Mapper<ConferenceDTO, Conference> conferenceDEMapper;
+    
+    @Autowired
+    private Merger merger;
+
     
     @GetMapping("/conferences")
     public List<ConferenceDTO> findAll(){
         return conferenceService.findAll().stream()
-                .map(converter.entityToDto(ConferenceDTO.class)).collect(toList());
+                .map(conferenceEDMapper::map).collect(toList());
     }
     
     @GetMapping("/conferences/{id}")
     public ConferenceDTO findById(@PathVariable Long id){
-        return converter.entityToDto(ConferenceDTO.class).apply(conferenceService.findById(id));
-//        return converter.entityToDto(ConferenceDTO.class).apply(new String()); // this gives error, but shouldnt  
+        return conferenceEDMapper.map(conferenceService.findById(id));
     }
     
     @PostMapping("/conferences")
     public ConferenceDTO save(@RequestBody ConferenceDTO conferenceDTO){
         conferenceDTO.setId(null);
         
-        return (ConferenceDTO) pipe(
-                converter.dtoToEntity(Conference.class), 
-                e -> conferenceService.save((Conference) e), 
-                converter.entityToDto(ConferenceDTO.class)
-        ).apply(conferenceDTO);
+        return update(conferenceDTO);
     }
     
     @PutMapping("/conferences")
     public ConferenceDTO update(@RequestBody ConferenceDTO conferenceDTO){
 
         return (ConferenceDTO) pipe(
-                converter.dtoToEntity(Conference.class),
-                e -> conferenceService.save((Conference) e),
-                converter.entityToDto(ConferenceDTO.class)
+                dto -> conferenceDEMapper.map((ConferenceDTO) dto), 
+                e -> conferenceService.save((Conference) e), 
+                e -> conferenceEDMapper.map((Conference) e)
         ).apply(conferenceDTO);
+    }
+    
+    @PatchMapping("/conferences/{id}")
+    public ConferenceDTO partialUpdate(@RequestBody ConferenceDTO conferenceDTO, @PathVariable Long id){
+        conferenceDTO.setId(null); // ignore if an id is given in the request body
+        conferenceDTO = merger.merge(conferenceDTO, this.findById(id));
+        
+        return this.update(conferenceDTO);
     }
     
     @DeleteMapping("/conferences/{id}")
--- src/main/java/com/by/organizeconference/controller/TopicController.java
+++ src/main/java/com/by/organizeconference/controller/TopicController.java
@@ -1,16 +1,24 @@
 package com.by.organizeconference.controller;
 
-import com.by.organizeconference.converter.Converter;
+import com.by.organizeconference.converter.MmConverter;
+import com.by.organizeconference.converter.OldConverter;
 import com.by.organizeconference.dto.TopicDTO;
 import com.by.organizeconference.entity.Topic;
-import static com.by.organizeconference.helper.Piper.pipe;
+import static com.by.organizeconference.utility.Piper.pipe;
 import com.by.organizeconference.service.TopicService;
+import static com.by.organizeconference.utility.MapToObject.mapToObject;
+import com.by.organizeconference.utility.Merger;
+import com.remondis.remap.Mapper;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import static java.util.stream.Collectors.toList;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,12 +38,19 @@ public class TopicController {
     private TopicService topicService;
     
     @Autowired
-    private Converter<Topic, TopicDTO> converter;
+    private Mapper<TopicDTO, Topic> topicDEMapper;
+    
+    @Autowired
+    private Mapper<Topic, TopicDTO> topicEDMapper;
+    
+    @Autowired
+    private Merger merger;
+
     
     @GetMapping("/topics")
     public List<TopicDTO> findAll(){
         return topicService.findAll().stream()
-                .map(converter.entityToDto(TopicDTO.class)).collect(toList());
+                .map(topicEDMapper::map).collect(toList());
     }
     
     @GetMapping("/topicEntities")
@@ -45,32 +60,37 @@ public class TopicController {
     
     @GetMapping("/topics/{id}")
     public TopicDTO findById(@PathVariable Long id){
-        return converter.entityToDto(TopicDTO.class).apply(topicService.findById(id));
+        return topicEDMapper.map(topicService.findById(id));
     }
     
     @PostMapping("/topics")
     public TopicDTO save(@RequestBody TopicDTO topicDTO){
         topicDTO.setId(null);
         
-        return (TopicDTO) pipe(
-                converter.dtoToEntity(Topic.class),
-                t -> topicService.save((Topic) t),
-                converter.entityToDto(TopicDTO.class)
-        ).apply(topicDTO);
+        return this.update(topicDTO);
     }
     
     @PutMapping("/topics")
     public TopicDTO update(@RequestBody TopicDTO topicDTO){
         return (TopicDTO) pipe(
-                converter.dtoToEntity(Topic.class),
-                t -> topicService.save((Topic) t),
-                converter.entityToDto(TopicDTO.class)
+                dto -> topicDEMapper.map((TopicDTO) dto),
+                e -> topicService.save((Topic) e),
+                e -> topicEDMapper.map((Topic) e)
         ).apply(topicDTO);
+    }
+    
+    @PatchMapping("/topics/{id}")
+    public TopicDTO partialUpdate(@RequestBody TopicDTO topicDTO, @PathVariable Long id){
+        topicDTO.setId(null);
+        topicDTO = merger.merge(topicDTO, this.findById(id));
+        
+        return this.update(topicDTO);
     }
     
     @DeleteMapping("/topics/{id}")
     public Map<String, Long> deleteById(@PathVariable Long id){
         return topicService.deleteById(id);
     }
+    
     
 }

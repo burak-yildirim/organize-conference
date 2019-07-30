12131db6476463f9e5cb3d35744137b5e711b329
burak-yildirim <yildirim.burak@protonmail.com>
2019-07-30 18:29:41 +0300

    DetailedSpeakerDTO added

---
+++ src/main/java/com/by/organizeconference/dto/DetailedSpeakerDTO.java
@@ -0,0 +1,87 @@
+package com.by.organizeconference.dto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author burakY
+ */
+public class DetailedSpeakerDTO {
+    
+    private Long id;
+    
+    private String fullName;
+    
+    private List<TopicDTO> topics;
+    
+    private String country;
+    
+    private String email;
+    
+    private String about;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public List<TopicDTO> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<TopicDTO> topics) {
+        this.topics = topics;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+    
+    private Map<String, Long> topicToMap(TopicDTO topicDTO){
+        Map<String, Long> ids = new HashMap<>();
+        ids.put("id", topicDTO.getId());
+        return ids;
+    }
+
+    @Override
+    public String toString() {
+        List<Map<String, Long>> printableTopics = topics.stream()
+                .map(this::topicToMap).collect(Collectors.toList());
+        return "DetailedSpeakerDTO{" + "id=" + id + ", fullName=" + fullName + ", topics=" + printableTopics + ", country=" + country + ", email=" + email + ", about=" + about + '}';
+    }
+    
+}
